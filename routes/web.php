08a5bd8b82497147ded2af04235051cc367a9860
Ignacio <ignaciofrizzera@gmail.com>
2020-06-18 09:59:46 -0300

    Cambio el redirect del middleware que verifica si el user logeado es admin o no. Pongo todas las rutas relacionadas a lo que hace el admin en un grupo de rutas bajo el middleware de admin.

--- routes/web.php
+++ routes/web.php
@@ -24,22 +24,25 @@ Route::get('/dashboard', 'HomeController@index')->name('home');
 
 Route::get('/userDashboard', 'UserController@index')->name('user');
 
-Route::get('/adminDashboard', 'AdminController@index')->name('admin');
+Route::group(['middleware' => 'admin'], function () {
+    
+    Route::get('/adminDashboard', 'AdminController@index')->name('admin');
 
-Route::get('/admin/addProducts', 'Product\AddProductController@index')->name('product');
+    Route::get('/admin/addProducts', 'Product\AddProductController@index')->name('product');
 
-Route::post('/adminDashboard-after-uploaded-product', 'Product\AddProductController@addProduct');
+    Route::post('/adminDashboard-after-uploaded-product', 'Product\AddProductController@addProduct');
 
-Route::get('/admin/showStock', 'Product\ShowStockController@searchAllStock');
+    Route::get('/admin/showStock', 'Product\ShowStockController@searchAllStock');
 
-Route::get('/admin/modifyProduct', 'Product\ModifyProductController@index');
+    Route::get('/admin/modifyProduct', 'Product\ModifyProductController@index');
 
-Route::get('/admin/modifyProduct-search', 'Product\ModifyProductController@searchProductByCode');
+    Route::get('/admin/modifyProduct-search', 'Product\ModifyProductController@searchProductByCode');
 
-Route::post('/adminDashboard-after-modified-product', 'Product\ModifyProductController@modifyProduct');
+    Route::post('/adminDashboard-after-modified-product', 'Product\ModifyProductController@modifyProduct');
 
-Route::get('/admin/deleteProduct', 'Product\DeleteProductController@index');
+    Route::get('/admin/deleteProduct', 'Product\DeleteProductController@index');
 
-Route::get('/admin/deleteProduct-search', 'Product\DeleteProductController@searchProductByCode');
+    Route::get('/admin/deleteProduct-search', 'Product\DeleteProductController@searchProductByCode');
 
-Route::post('/adminDashboard-after-deleted-product', 'Product\DeleteProductController@deleteProduct');
+    Route::post('/adminDashboard-after-deleted-product', 'Product\DeleteProductController@deleteProduct');
+});
