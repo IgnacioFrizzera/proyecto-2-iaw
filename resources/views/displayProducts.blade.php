5c3010ba6173d2b0fcc2135b9faff2e41684021c
Ignacio <ignaciofrizzera@gmail.com>
2020-06-19 16:26:08 -0300

    Remuevo value que había dejado de testeo

--- resources/views/displayProducts.blade.php
+++ resources/views/displayProducts.blade.php
@@ -15,7 +15,7 @@
                                         <div class="card">
                                             <?php $target_name = $value->name.$value->code; ?>
                                             <img src="{{ asset('uploads/temp/products/'.$target_name)}}" alt="Image" width="250px" height="250px">
-                                            <h1>Tailored Jeans</h1>
+                                            <h1> {{$value->name}} </h1>
                                             <p class="price"> ${{ $value->price }} </p>
                                             <p>{{ $value->description }}</p>
                                             <input type="hidden" name="code" value = {{$value->code}} >
