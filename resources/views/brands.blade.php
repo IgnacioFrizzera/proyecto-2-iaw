284f48a4007010f3e502de8932bc69bdd0a6ab79
Ignacio <ignaciofrizzera@gmail.com>
2020-06-25 17:11:36 -0300

    Fix del search de brands

--- resources/views/brands.blade.php
+++ resources/views/brands.blade.php
@@ -17,7 +17,7 @@
                         @foreach ($shopBrands as $value)
                             <div class="col-lg-3 col-sm-4" style="padding-bottom: 16px">
                                 <form action=" {{ route('search-products-by-brand') }} " method="GET">
-                                    <input type="hidden" name="brand" value={{$value->brand}}>
+                                    <input type="hidden" name="brand" value="{{$value->brand}}">
                                     <button type="submit" class="button1">{{$value->brand}}</a>
                                 </form>
                             </div>
